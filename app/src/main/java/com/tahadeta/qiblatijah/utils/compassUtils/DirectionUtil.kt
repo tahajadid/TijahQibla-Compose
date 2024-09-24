dc0467a0d0f12b9e729bf3c5a097fbf9d172fafa
Taha Jadid <jadid.taha@waroom.com>
2024-09-24 09:58:18 +0100

    Add calculate of bearing

--- app/src/main/java/com/tahadeta/qiblatijah/utils/compassUtils/DirectionUtil.kt
+++ app/src/main/java/com/tahadeta/qiblatijah/utils/compassUtils/DirectionUtil.kt
@@ -18,3 +18,21 @@ fun getDirectionsLabel(
         in 293..337 -> context.getString(R.string.northwest)
         else -> ""
     }
+
+
+fun calculateBearing(
+    lat1: Double,
+    lon1: Double,
+    lat2: Double,
+    lon2: Double): Double {
+    val phi1 = Math.toRadians(lat1)
+    val phi2 = Math.toRadians(lat2)
+    val deltaLambda = Math.toRadians(lon2 - lon1)
+
+    val y = Math.sin(deltaLambda) * Math.cos(phi2)
+    val x = Math.cos(phi1) * Math.sin(phi2) -
+            Math.sin(phi1) * Math.cos(phi2) * Math.cos(deltaLambda)
+
+    val theta = Math.atan2(y, x)
+    return (Math.toDegrees(theta) + 360) % 360 // Normalize to 0-360 degrees
+}
