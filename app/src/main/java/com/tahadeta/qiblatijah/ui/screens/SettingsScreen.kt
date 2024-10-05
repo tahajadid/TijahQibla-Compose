c34481de29d39e476c6385a26abe913dc15b3c40
Taha Jadid <jadid.taha@waroom.com>
2024-10-05 13:00:16 +0100

    Add session

--- app/src/main/java/com/tahadeta/qiblatijah/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/tahadeta/qiblatijah/ui/screens/SettingsScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.tahadeta.qiblatijah.BuildConfig
 import com.tahadeta.qiblatijah.MainActivity
 import com.tahadeta.qiblatijah.R
 import com.tahadeta.qiblatijah.ui.theme.ColorCorrect
